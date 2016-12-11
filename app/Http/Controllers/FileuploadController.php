ddb54d7fdc4711e59469c5530719b87a8f116371
khaled ebdelli <khaled.ebdelli@esprit.tn>
2016-12-11 22:58:01 +0100

    file upload modif

--- app/Http/Controllers/FileuploadController.php
+++ app/Http/Controllers/FileuploadController.php
@@ -18,10 +18,10 @@ class FileuploadController extends Controller
       $name = $file->getClientOriginalName();
       $size = $file->getSize();
       $extension = $file->getClientOriginalExtension();
-      $destinationPath = 'uploads';
+      $destinationPath = 'uploads'.Auth::user()->id;
       $image_name = Auth::user()->id;
-      $file->move('uploads', $image_name);
-      $image = Image::make(sprintf('uploads/%s', $image_name))->resize(200, 200)->save();
+      $file->move($destinationPath, $image_name);
+      $image = Image::make(sprintf($destinationPath.'/%s', $image_name))->resize(200, 200)->save();
       return View('file.file' , array('name' =>$name ,'named' =>$image_name ,'size' =>$size, 'extension' =>$extension ));
     }
 }
